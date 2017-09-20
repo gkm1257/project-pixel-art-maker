ab80cb9865e138e533d68b3d4dfc1ed9bbe74057
gkm1257 <gkm1257@gmail.com>
2017-09-20 22:46:58 +0800

    feat: Add makeGrid function

--- designs.js
+++ designs.js
@@ -6,5 +6,21 @@
 function makeGrid() {
 
 // Your code goes here!
+  let height = $('#input_height').val();
+  let width = $('#input_width').val();
+  // console.log(height,width);
+  for (let r = 0; r < height; r++) {
+    $('#pixel_canvas').append('<tr></tr>');
+    let currentRow = $('#pixel_canvas tr').eq(r);
+    currentRow.attr('id','r'+r);
+    for (let c = 0; c < width; c++) {
+        currentRow.append('<td></td>');
+        let currentCol = $('#pixel_canvas td').eq(c);
+        currentCol.attr('id','c'+c);
+    }
+  }
 
 }
+
+// test makeGrid function
+$(makeGrid);
